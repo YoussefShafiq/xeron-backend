adbcdd478d25a484dada464071e896e30b8fd743
youssef shafek <youssefmsa616@gmail.com>
2026-04-15 15:16:26 +0200

    Refactor database connection handling to use try-catch for error management

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -4,13 +4,12 @@ import { DB_URL } from '../../configs/app.config.js'
 
 
 const testDbConncection = async () => {
-    await connect(DB_URL).then(
+    try {
+        await connect(DB_URL)
         console.log('connected to DB successfully')
-    ).catch(
-        err => {
-            console.log('error in connection to DB', err);
-        }
-    )
+    } catch (err) {
+        console.log('error in connection to DB', err);
+    }
 }
 
 export default testDbConncection
